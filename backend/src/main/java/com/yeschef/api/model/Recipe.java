641dbb603157ab6689d147c854ca926daec07df6
car-z <carolinezzhao@gmail.com>
2026-02-16 21:16:03 -0500

    added beginnings of Recipe table

---
+++ backend/src/main/java/com/yeschef/api/model/Recipe.java
@@ -0,0 +1,18 @@
+package com.yeschef.api.model;
+
+import jakarta.persistence.*;
+// import java.util.List;
+// import org.hibernate.annotations.JdbcTypeCode;
+// import org.hibernate.type.SqlTypes;
+
+@Entity
+@Table(name ="RECIPES")
+public class Recipe {
+    
+    @Id // this is the primary ID
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // will auto-generate ID
+    private Long id;
+
+    @Column(nullable = false) // this column cannot be null
+    private String title;
+}
