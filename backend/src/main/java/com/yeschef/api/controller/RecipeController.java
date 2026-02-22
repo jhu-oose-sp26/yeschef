2018cc7027eacee66a2da6be74f37595f5c11127
Anna Andrade <aandra12@jh.edu>
2026-02-22 01:50:46 -0500

    add GET controller for get recipe by id and get all recipes in the database. had to update Recipe.java to fix supabase mapping, not sure if will have to do this with the rest of the model classes. also updated some of the lazy mappings so it would actually return whats in the db

--- backend/src/main/java/com/yeschef/api/controller/RecipeController.java
+++ backend/src/main/java/com/yeschef/api/controller/RecipeController.java
@@ -4,9 +4,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+import java.util.List;
 
 import com.yeschef.api.model.Recipe;
 import com.yeschef.api.repository.RecipeRepository;
@@ -25,7 +29,26 @@ public class RecipeController {
         this.recipeRepository = recipeRepository;
     }
 
+    // Handle GET requests to /recipes
+    // Return one recipe by id
+    @GetMapping("/{id}")
+    public ResponseEntity<Recipe> getRecipe(@PathVariable Long id) {
+        // call repo to find recipe by id
+        Optional<Recipe> recipeMaybe = recipeRepository.findById(id);
+        // return recipe or null if not found
+        if (recipeMaybe.isPresent()) {
+            return ResponseEntity.ok(recipeMaybe.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 
+    @GetMapping
+    public ResponseEntity<List<Recipe>> getAllRecipes() {
+        // call repo to return all recipes from database and return
+        List<Recipe> recipes = recipeRepository.findAll();
+        return ResponseEntity.ok(recipes);
+    }
 
     // Handles POST requests to /recipes
     // Expects a Recipe JSON in the request body, converts it to a Recipe entity, saves it to the database, and returns the saved Recipe.
